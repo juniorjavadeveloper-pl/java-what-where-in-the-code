bbda1b23bc9d6f3a56c228bb97b8562817e591d1
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-03-11 22:12:32 +0100

    Add separate util class with static method for Calculator implementation

---
+++ src/pl/juniorjavadeveloper/java/wwitc/CalculatorPhaseOne.java
@@ -0,0 +1,10 @@
+package pl.juniorjavadeveloper.java.wwitc;
+
+public class CalculatorPhaseOne {
+    public static int sum(int a, int b) {
+        System.out.println("sum(" + a + ", " + b + ")");
+        int sum = a + b;
+        System.out.println("sum=" + sum);
+        return sum;
+    }
+}
---
+++ src/pl/juniorjavadeveloper/java/wwitc/MainCalculatorPhaseOne.java
@@ -0,0 +1,24 @@
+package pl.juniorjavadeveloper.java.wwitc;
+
+import java.util.Scanner;
+
+public class MainCalculatorPhaseOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj pierwszą liczbę:");
+        int a = scanner.nextInt();
+        System.out.println("Podaj drugą liczbę:");
+        int b = scanner.nextInt();
+
+        System.out.println("Jakie działanie wykonać?");
+        System.out.println("1. Dodawanie");
+        int operation = scanner.nextInt();
+
+        int result = 0;
+        if (operation == 1) {
+            result = CalculatorPhaseOne.sum(a, b);
+        }
+        System.out.println("Wynik działania: " + result);
+    }
+}
